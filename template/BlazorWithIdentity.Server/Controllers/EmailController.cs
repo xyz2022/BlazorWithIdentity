d25ec44f83e5e57aa309a28294928cab77a5b5b9
XYZ2022 <teamloruw@outlook.com>
2021-10-13 14:56:17 +1100

    Added Dummy Email Inbox Controller

---
+++ template/BlazorWithIdentity.Server/Controllers/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using BlazorWithIdentity.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWithIdentity.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class EmailController : Controller
+    {
+        private static string[] Summaries2 = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<InboxEmail> InboxEmails()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new InboxEmail
+            {
+                Subject = "Test Subject",
+                Sender = "Test Sender",
+                Date = DateTime.Now.AddDays(index)
+
+            });
+        }
+    }
+}
---
+++ template/BlazorWithIdentity.Shared/EmailList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWithIdentity.Shared
+{
+    public class InboxEmail
+    {
+        public string Subject { get; set; }
+        public string Sender { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
